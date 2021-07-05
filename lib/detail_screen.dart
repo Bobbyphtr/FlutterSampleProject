c896d522e0b39f156044f3773005a09ab7adbc3a
Bobbyphtr <bobbypehtrus2@gmail.com>
2021-07-05 08:28:25 +0700

    Added Web View

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sample_project/model/tourism_place.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class DetailScreen extends StatelessWidget {
   TourismPlace model;
@@ -9,50 +10,194 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: SingleChildScrollView(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              if (constraints.maxWidth >= 800) {
+                return DetailPageWeb(model: model);
+              }
+              return DetailPageMobile(model: model);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailPageWeb extends StatelessWidget {
+  TourismPlace model;
+
+  DetailPageWeb({required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Stack(
-            children: [
-              Hero(
-                tag: "gambar",
-                child: Image.asset(model.imageAsset),
-              ),
-              SafeArea(
-                child: IconButton(
-                    icon: Icon(Icons.arrow_back),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    }),
-              )
-            ],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Wisata Bandung".toUpperCase(),
+            textAlign: TextAlign.start,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
           ),
           SizedBox(
-            height: 8,
+            height: 16,
           ),
-          Container(
-              child: Text(
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      clipBehavior: Clip.antiAlias,
+                      child: Image(
+                        image: AssetImage(kIsWeb
+                            ? model.imageAsset
+                            : "assets/" + model.imageAsset),
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: model.imageUrls.map((url) {
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 8, 8, 0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(url),
+                              ),
+                            );
+                          }).toList()),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        model.name.toUpperCase(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(Icons.calendar_today, color: Colors.black),
+                          Text(model.openDays),
+                          Spacer(),
+                          FavoriteButton(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.watch_later_outlined),
+                          Text(model.openTime),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.monochrome_photos_outlined),
+                          Text(model.ticketPrice),
+                        ],
+                      ),
+                      Text(model.description)
+                    ],
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DetailPageMobile extends StatelessWidget {
+  TourismPlace model;
+
+  DetailPageMobile({required this.model});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Stack(
+          children: [
+            Hero(
+                tag: "gambar",
+                child: Image(
+                  image: AssetImage(
+                      // This checks if runs on web or else. If runs on web, the "assets/" are added autmatically
+                      kIsWeb ? model.imageAsset : "assets/" + model.imageAsset),
+                )),
+            // Back Button
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      child: IconButton(
+                          color: Colors.white,
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          }),
+                    ),
+                    FavoriteButton()
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Container(
+          child: Text(
             model.name,
             textAlign: TextAlign.center,
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          )),
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
                   children: [
                     Icon(Icons.calendar_today),
                     SizedBox(
                       height: 8,
                     ),
-                    Text(model.openDays)
+                    Text(
+                      model.openDays,
+                      textAlign: TextAlign.center,
+                    )
                   ],
                 ),
-                Column(
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
                   children: [
                     Icon(Icons.watch_later_outlined),
                     SizedBox(
@@ -61,7 +206,10 @@ class DetailScreen extends StatelessWidget {
                     Text(model.openTime)
                   ],
                 ),
-                Column(
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
                   children: [
                     Icon(Icons.monetization_on_outlined),
                     SizedBox(
@@ -70,33 +218,59 @@ class DetailScreen extends StatelessWidget {
                     Text(model.ticketPrice)
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-          Container(
-              padding: const EdgeInsets.only(top: 8),
-              child: Text(
-                model.description,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15),
-              )),
-          Container(
-              padding: const EdgeInsets.only(top: 8),
-              height: 200,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: model.imageUrls.map((url) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.network(url),
-                    ),
-                  );
-                }).toList(),
-              ))
-        ],
+        ),
+        Container(
+            padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+            child: Text(
+              model.description,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15),
+            )),
+        Container(
+            padding: const EdgeInsets.only(top: 8),
+            height: 200,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: model.imageUrls.map((url) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(url),
+                  ),
+                );
+              }).toList(),
+            ))
+      ],
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
       ),
-    )));
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:sample_project/model/tourism_place.dart';
 import 'detail_screen.dart';
@@ -20,29 +21,144 @@ class ListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          title: Text("Wisata Bandung"),
-        ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            final TourismPlace place = tourismPlaceList[index];
-            return InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Text("Wisata Bandung"),
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          // If less than 600
+          if (constraints.maxWidth <= 600) {
+            return PlaceList();
+          }
+          // if less than 1200
+          else if (constraints.maxWidth <= 1200) {
+            return PlaceGridList(columnCount: 4);
+          }
+          // if more than 1200
+          else {
+            return PlaceGridList(columnCount: 6);
+          }
+        },
+      ),
+    );
+  }
+}
+
+class PlaceList extends StatelessWidget {
+  const PlaceList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final TourismPlace place = tourismPlaceList[index];
+        return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
                     return DetailScreen(
                       model: place,
                     );
-                  }));
+                  },
+                ),
+              );
+            },
+            child: PlaceItemCard(
+              imageAssetUrl: place.imageAsset,
+              placeDescription: place.location,
+              placeName: place.name,
+            ));
+      },
+      itemCount: tourismPlaceList.length,
+    );
+  }
+}
+
+class PlaceGridList extends StatelessWidget {
+  int columnCount;
+
+  PlaceGridList({required this.columnCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.count(
+            crossAxisCount: columnCount,
+            children: tourismPlaceList.map((place) {
+              return InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return DetailScreen(
+                          model: place,
+                        );
+                      },
+                    ),
+                  );
                 },
-                child: PlaceItemCard(
-                  imageAssetUrl: place.imageAsset,
-                  placeDescription: place.location,
-                  placeName: place.name,
-                ));
-          },
-          itemCount: tourismPlaceList.length,
-        ));
+                child: PlaceItemGridCard(
+                    imageAssetUrl: place.imageAsset,
+                    placeName: place.name,
+                    placeDescription: place.location),
+              );
+            }).toList()),
+      ),
+    );
+  }
+}
+
+class PlaceItemGridCard extends StatelessWidget {
+  String imageAssetUrl;
+  String placeName;
+  String placeDescription;
+
+  PlaceItemGridCard(
+      {required this.imageAssetUrl,
+      required this.placeName,
+      required this.placeDescription});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shadowColor: Colors.grey,
+      elevation: 3,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Image(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                  kIsWeb ? imageAssetUrl : "assets/" + imageAssetUrl),
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+              child: Text(
+                placeName,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              )),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(8, 4, 8, 8),
+              child: Text(
+                placeDescription,
+                style: TextStyle(fontSize: 11),
+              )),
+        ],
+      ),
+    );
   }
 }
 
@@ -66,7 +182,13 @@ class PlaceItemCard extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: Hero(tag: "gambar", child: Image.asset(imageAssetUrl)),
+              child: Hero(
+                  tag: "gambar",
+                  child: Image(
+                    image: AssetImage(
+                        //  On web version the assets are stored in "assets/" folder already.
+                        kIsWeb ? imageAssetUrl : "assets/" + imageAssetUrl),
+                  )),
             ),
             Expanded(
                 flex: 2,
